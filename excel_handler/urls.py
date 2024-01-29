82726771fce0cafecacd89a1ea7959054e17cf5c
Aashutosh-kumar <oficialashu@gmail.com>
2024-01-29 20:02:38 +0530

    Second commit

--- excel_handler/urls.py
+++ excel_handler/urls.py
@@ -2,8 +2,10 @@
 
 from django.urls import path
 from . import views
+from .views import display_excel_data
 
 urlpatterns = [
     path('upload/', views.upload_excel, name='upload_excel'),
     path('details/', views.excel_detail, name='excel_detail'),
+    path('display-excel/', display_excel_data, name='display_excel_data'),
 ]
--- excel_handler/views.py
+++ excel_handler/views.py
@@ -6,6 +6,7 @@ from .forms import ExcelUploadForm
 from .models import ExcelFile
 from google.cloud import storage
 from django.conf import settings
+import openpyxl
 
 
 def upload_excel(request):
@@ -23,7 +24,7 @@ def upload_excel(request):
         blob.upload_from_file(file)
         
         
-        return redirect('excel_detail')  
+        return redirect('display_excel_data')  
 
     else:
         # If the request method is not POST or no file is provided, render the form
@@ -36,3 +37,31 @@ def excel_detail(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'excel_detail.html', {'page_obj': page_obj})
+
+def display_excel_data(request):
+    client = storage.Client.from_service_account_json(settings.GOOGLE_CLOUD_STORAGE_JSON_KEY_FILE)
+    bucket = client.bucket(settings.GOOGLE_CLOUD_STORAGE_BUCKET)
+
+
+    # List all blobs/files in the bucket
+    blobs = list(bucket.list_blobs())
+    print(blobs)
+    # Sort blobs by creation time (most recent first)
+    blobs.sort(key=lambda x: x.time_created, reverse=True)
+
+    # Get the most recent blob (assumes at least one file exists)
+    most_recent_blob = blobs[0]
+
+    # Download the most recent blob to a local temporary location
+    temp_file_path = 'most_recent_file.xlsx'
+    most_recent_blob.download_to_filename(temp_file_path)
+
+    # Read the contents of the downloaded file
+    wb = openpyxl.load_workbook(temp_file_path)
+    sheet = wb.active
+
+    data = []
+    for row in sheet.iter_rows(values_only=True):
+        data.append(row)
+
+    return render(request, 'display_excel_data.html', {'data': data})
